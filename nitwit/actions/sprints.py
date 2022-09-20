d8251d34b18c884ba6464fe50dc887761890274e
Luke Dupin <lukedupin@gmail.com>
2022-09-19 22:44:38 -0700

    Sprint print out

--- nitwit/actions/sprints.py
+++ nitwit/actions/sprints.py
@@ -1,19 +1,29 @@
 from nitwit.storage import sprints as sprints_mod
+from nitwit.storage import tickets as tickets_mod
 from nitwit.helpers import util
 
 import random, os
 
 
 def handle_sprints( parser, options, args, settings ):
+    tickets = {t.uid: t for t in tickets_mod.import_tickets( settings )}
+
     # Print out the sprints
     if len(args) < 2:
-        sprints = sprints_mod.import_sprints( settings )
-        if len(sprints) <= 0:
+        sprints = sprints_mod.import_latest_sprints( settings, filter_owners=[settings['username']])
+        if len(sprints) != 1:
             print( "No sprints found. Try creating one." )
 
         # Dump the sprints to the screen
-        for sprint in sprints:
-            print(f'#{sprint.name}')
+        print("My active tickets")
+        for uid in sprints[0].ticket_uids:
+            title = ""
+            cat = ""
+            if (ticket := tickets.get(uid)) is not None:
+                title = ticket.title
+                cat = f'^{ticket.category}'
+
+            print(f'    ${uid}  {cat.ljust(16)} {title[:64]}')
 
         return None
 
